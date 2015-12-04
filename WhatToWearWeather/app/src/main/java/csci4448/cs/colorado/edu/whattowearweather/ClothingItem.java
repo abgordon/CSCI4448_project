25a2eee4a531aa0557dbe81a42ad41d16525d246
Dan <djzurawski@gmail.com>
2015-12-04 15:22:15 -0700

    beginning recommender

---
+++ WhatToWearWeather/app/src/main/java/csci4448/cs/colorado/edu/whattowearweather/ClothingItem.java
@@ -0,0 +1,15 @@
+package csci4448.cs.colorado.edu.whattowearweather;
+
+import java.lang.reflect.Type;
+
+/**
+ * Created by dan on 12/4/15.
+ */
+public class ClothingItem {
+
+    private Types.Temp mTempType;
+    private Types.Precip mPrecipType;
+    private Types.BodyPart mBodyPart;
+    private int mGenderType;
+
+}
