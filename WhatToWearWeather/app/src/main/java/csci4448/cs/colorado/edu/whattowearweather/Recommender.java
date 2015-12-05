3b7cc5c53f3676efca18cc40f44d65ad247478ac
Andrew Gordon <abgordon@colorado.edu>
2015-12-04 17:03:24 -0700

    updated recommender

--- WhatToWearWeather/app/src/main/java/csci4448/cs/colorado/edu/whattowearweather/Recommender.java
+++ WhatToWearWeather/app/src/main/java/csci4448/cs/colorado/edu/whattowearweather/Recommender.java
@@ -1,27 +1,32 @@
 package csci4448.cs.colorado.edu.whattowearweather;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
+import static android.database.sqlite.SQLiteDatabase.openOrCreateDatabase;
+
 /**
  * Created by dan on 12/4/15.
  */
 public class Recommender {
 
-    private ClothingItem mFeet;
-    private ClothingItem mLegs;
+//    private ClothingItem mFeet;
+//    private ClothingItem mLegs;
     private ClothingItem mChest;
-    private ClothingItem mHands;
-    private ClothingItem mHead;
-    private ClothingItem mOther;
+//    private ClothingItem mHands;
+//    private ClothingItem mHead;
+//    private ClothingItem mOther;
 
     private double mTemp;
 
     private Types.Temp mTempType;
 
+    int gender = 0;
 
-    public Recommender(Forecast forecast) {
+    public ArrayList<ClothingItem> Recommender(Forecast forecast) {
         mTemp = forecast.getTemp();
-
-        //dummy data for test
-
 
 
         mTempType = Types.Temp.VERY_COLD;
@@ -31,6 +36,28 @@ public class Recommender {
                 mTempType = t;
             }
         }
-        System.out.println(mTempType);
+
+        SQLiteDatabase clothes_db = openOrCreateDatabase("clothes_db", MODE_PRIVATE, null);
+        String request = new String("Select * from TutorialsPoint where Temperature=%d and Gender=%d and BodyPart=CHEST", mTempType, gender);
+        Cursor resultSet = clothes_db.rawQuery(request, null);
+
+
+        ArrayList<ClothingItem> recommendations = new ArrayList<ClothingItem>();
+        ClothingItem current;
+        while (resultSet.next()) {
+            //create clothing items and return them
+            int temp = resultSet.getInt("Temperature");
+            int gender = resultSet.getInt("Gender")
+            String name = resultSet.getString("Name");
+            String bodypart = new String("CHEST");
+            Types.Precip precip = resultSet.getString("Precipitation");
+
+
+            current = new ClothingItem(temp, precip, bodypart, gender, name);
+            recommendations.add(current);
+        }
+        resultSet.close();
+
+        return recommendations;
+
     }
-}
